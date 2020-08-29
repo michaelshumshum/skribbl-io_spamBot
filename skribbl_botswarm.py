ad4aaa156d5092ae6784bd09ef1e6abed8e8fadc
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2020-08-29 22:29:03 +0800

    Update skribbl_botswarm.py

--- skribbl_botswarm.py
+++ skribbl_botswarm.py
@@ -13,6 +13,12 @@ from selenium.webdriver.chrome.options import Options
 driverDirectory = '/Users/michaelshum/Desktop/dumb_python_scripts/WebDrivers/chromedriver' #Put the directory of the chromedriver here.
 url = 'https://skribbl.io/'
 
+def chatupdate(queue):
+	chat = driver.find_element_by_xpath('//*[@id="boxMessages"]').text
+	chatlist = chat.splitlines()
+
+	queue.put(['chat',chatlist])
+
 def chatSend(theString):
 	driver.find_element_by_xpath('//*[@id="inputChat"]').send_keys(theString)
 	driver.find_element_by_xpath('//*[@id="inputChat"]').submit()
@@ -27,33 +33,44 @@ def randomString(stringLength):
 	lettersAndDigits = string.ascii_letters + string.digits
 	return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
 
+def botrestart(queue):
+	driver.close()
+	driver.quit()
+	queue.put([i,'Offline',round(time.time())])
+
+def botinit(queue):
+	global driver
+	chrome_options = Options()
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--mute-audio")
+	driver = webdriver.Chrome(executable_path=driverDirectory, chrome_options=chrome_options) #Change the directory to where your chromedriver is located.
+	driver.implicitly_wait(25) 
+	queue.put([i,'Initializing',round(time.time())])
+	driver.get(url)
+
 def botfunc(queue,num):
+	global driver
 	while True:
 		while True:
-			global driver
-			chrome_options = Options()
-			chrome_options.add_argument("--headless")
-			chrome_options.add_argument("--mute-audio")
-			driver = webdriver.Chrome(executable_path=driverDirectory, chrome_options=chrome_options) #Change the directory to where your chromedriver is located.
-			driver.implicitly_wait(15) 
-			queue.put([i,'Initializing'])
-			driver.get(url)
+			botinit(queue)
 			try:
 				driver.find_element_by_xpath('//*[@id="inputName"]').send_keys('spamBot') #Feel free to change the name.
 				time.sleep(1)
+				#driver.find_element_by_xpath('/html/body/div[2]/div/a[1]').click()
 				driver.find_element_by_xpath('//*[@id="formLogin"]/button[1]').click()
-				time.sleep(3)
-				while True:
-					queue.put([i,'Searching'])
+			except:
+				botrestart(queue)
+				break
+			_time = round(time.time())
+			while True:
+					queue.put([i,'Searching',_time])
 					try:
-						try:
-							playerCount = (driver.find_element_by_xpath('//*[@id="containerGamePlayers"]')).size['height'] / 48
-						except:
-							playerCount = 1
+						time.sleep(5)
+						playerCount = (driver.find_element_by_xpath('//*[@id="containerGamePlayers"]')).size['height'] / 48
 						if playerCount > 1:
+							_time = round(time.time())
 							chatSend('Hey there. I am spamBot. I am here to make this game hell for you.') #Feel free to change this line.
 							chatSend('Please enjoy hell with me! *If you copy me, you will probably get kicked.') #Feel free to change this line.
-							playerCountStore = ''
 							time.sleep(2.5)
 							while True:
 								try:
@@ -62,35 +79,31 @@ def botfunc(queue,num):
 									except:
 										continue
 									chatSend(randomString(random.randint(1,99)))
+									#chatupdate(queue)
 									time.sleep(1)
 									playerCount = (driver.find_element_by_xpath('//*[@id="containerGamePlayers"]')).size['height'] / 48
-									if playerCount != playerCountStore:
-										queue.put([i,'In Game with {} players'.format(playerCount)])
-										playerCountStore = playerCount
+									queue.put([i,'In game with {} players'.format(playerCount),_time])
 									if playerCount < 2:
 										relogin()
+										queue.put(['chat','Disconnected'])
 										break
 									if (driver.find_element_by_xpath('//*[@id="overlay"]/div').get_attribute('style') == "bottom: 0%;") and (driver.find_element_by_xpath('//*[@id="overlay"]/div/div[1]').text == "Choose a word"):
 										chatSend("Sorry, I wasn't made to draw. I was made only to spam.")
 										time.sleep(0.2)
 										chatSend('Your terror with me is now over. Take care now!')
 										relogin()
+										_time = round(time.time())
+										#queue.put(['chat',['Disconnected']])
 										break
 								except:
 									relogin()
+									_time = round(time.time())
 									break
 						else: 
 							relogin()
 					except:
-						driver.close()
-						driver.quit()
-						queue.put([i,'Offline'])
+						botrestart(queue)
 						break
-			except:
-				driver.close()
-				driver.quit()
-				queue.put([i,'Offline'])
-				break
 
 bots = []
 
@@ -103,37 +116,97 @@ for i in range(0,botCount):
 	b = Process(target=botfunc, name='Bot {}'.format(i+1), args=(q,i,))
 	bots.append('Bot {}'.format(i+1))
 	b.start()
-	time.sleep(0.1)
+	time.sleep(0.75)
+
+#GUI Stuff
 
 def listening(conn):
 	statuses = []
+	chat = []
+	times = []
+	ingame = []
 	for i in range(0,botCount):
 		statuses.append('Offline')
+		ingame.append('False')
+		times.append(0.0)
 	while True:
-		status = q.get()
-		num = status[0]
-		line = status[1]
-		statuses[num] = line
-		conn.send(statuses)
+		recieve = q.get()
+		num = recieve[0]
+		if num == 'chat':
+			chat = []
+			chat = recieve[1]
+			conn.send([chat,'chat'])
+		else:
+			line = recieve[1]
+			if 'In game' in line:
+				_ingame = True
+			else:
+				_ingame = False
+			_time = recieve[2]
+			times[num] = _time
+			statuses[num] = line
+			ingame[num] = _ingame
+			conn.send([statuses,'status',times,ingame])
 
 l = Process(target=listening, name='listening_process', args=(child,))
 l.start()
 
-def guifunc(*args):
-	gui = npyscreen.Form(name='skribbl.io - spamBot')
-	btstat = gui.add(npyscreen.SimpleGrid, name='bot statuses')
+class status(npyscreen.BoxTitle):
+	_contained_widget = npyscreen.SimpleGrid
+	entry_widget = npyscreen.SimpleGrid
 
+class logs(npyscreen.BoxTitle):
+	_contained_widget = npyscreen.Pager
+	entry_widget = npyscreen.Pager
+
+class choice(npyscreen.BoxTitle):
+	_contained_widget = npyscreen.MultiLineAction
+	entry_widget = npyscreen.MultiLineAction
+
+class Form(npyscreen.Form):
+	def create(self):
+		rows, columns = os.popen('stty size').read().split()
+		rows = int(rows)
+		columns = int(columns)
+		logsheight = -(rows-(botCount+5))
+		self.btstat = self.add(status, name='bot statuses',editable=False,max_height=botCount+3)
+		self.choice = self.add(choice, name='choice',editable=True,max_width=columns/12)
+		self.logs = self.add(logs, name='logs',editable=False,relx=columns/12+2,rely=logsheight)
+	def valueupdate(self):
+		if recieved[1] == 'status':
+			self.btstat.values = [['BOT NUMBER', 'STATUS', 'TIME IN STATUS']]
+			for x in range(0,botCount):
+				row = []
+				times = recieved[2]
+				statuses = recieved[0]
+				row.append(bots[x])
+				row.append(statuses[x])
+				row.append(round(time.time())- times[x])
+				self.btstat.values.append(row)
+				ingame = recieved[3]
+			for x in range(0,botCount):
+				if (ingame[x] == True) and (bots[x] not in botsingame):
+					botsingame.append(bots[x])
+				elif (bots[x] in botsingame) and (ingame[x] == False):
+					botsingame.remove(bots[x])
+				botsingame.sort()
+		if recieved[1] == 'chat':
+			self.logs.values = recieved[0]
+		elif 'Disconnected' in recieved[2]:
+			self.logs.values = []
+		self.choice.values = botsingame
+
+def guifunc(*args):
+	global botsingame
+	botsingame = []
+	gui = Form(name='skribbl.io - spamBot')
 	while True:
-		statuses = parent.recv()
-		btstat.values = []
-		for x in range(0,botCount):
-			row = []
-			row.append(bots[x])
-			row.append(statuses[x])
-			btstat.values.append(row)
+		global recieved
+		recieved = parent.recv()
+		gui.valueupdate()
 		gui.display()
 
-npyscreen.wrapper_basic(guifunc)
+print npyscreen.wrapper_basic(guifunc)
 
 for bot in bots:
 	bot.join()
